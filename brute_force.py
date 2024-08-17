2f9f71c1b8a9c75689070b0b590e41d1062b9c0a
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-08-17 00:02:21 -0500

    docs: fixed counting mistake in my analysis

--- brute_force.py
+++ brute_force.py
@@ -2,24 +2,24 @@ import timeit;
 def mcd_fuerza_bruta(a, b):
 
     menor = min(a, b) # 1 + 1
-    for i in range(menor, 0, -1): ## 1 + n(1 + INSIDE + 1)
-        if a % i == 0 and b % i == 0: ## 1 + 1 + 2 + 1 + 1 + 2
+    for i in range(menor, 0, -1): ## 1 + n(1 + INSIDE + 2)
+        if a % i == 0 and b % i == 0: ## 2 + 1  +2 + 1
             return i ## 1
     return 1 ## 1
 
 ## Worst and Average Case
-## 2 + 1 + n( 1 + 1 + 2 + 1 + 1 + 2) + 1 = 4 + 8n
+## 2 + 1 + n(1 + 6 + 2) + 1 = 4 +9n
 ## Better Case
-## 1 + 1 + n(1 + 1 + 1) = 2 + 3n
+## 2 + 1 + 1 + 1= 5 
 
 
 def mcd_euclides(a, b):
-        while b != 0:  ## 1 + 2 + n(INSIDE)
-            a, b = b, a % b ## 1 + 1
+        while b != 0:  ## 1 + 2 + n(2 + INSIDE)
+            a, b = b, a % b ## 1 + 1 + 1
         return a ## 1
     
 ## Worst and Average Case
-## 2 + 1 + n(1 + 1 + 1) + 1 = 4 + 3n
+## 2 + 1 + n(1 + 1 + 1 + 2) + 1 = 4 + 5n
 ## Better Case
 ## 2 + 1 + 1 = 4
 
@@ -44,8 +44,8 @@ def lower_time():
     else:
         return "Euclides"
 
-print("Euclides:", compare_euclides(10000000,1000000000000))
-print("Fuerza Bruta:", compare_fuerza_bruta(10000000,10000000))
+print("Euclides:", compare_euclides(10000000000000,1000000000000))
+print("Fuerza Bruta:", compare_fuerza_bruta(1000000000000,1000000000000))
 
 
 ##Todo: Compare these algorithms with the time module
