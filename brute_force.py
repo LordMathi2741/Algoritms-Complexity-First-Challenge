6ae0804126922e4bf3b22134d83d2d426fc275a6
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-08-16 20:22:07 -0500

    feat(compare): added compare function

--- brute_force.py
+++ brute_force.py
@@ -1,4 +1,6 @@
+import time;
 def mcd_fuerza_bruta(a, b):
+
     menor = min(a, b) # 1 + 1
     for i in range(menor, 0, -1): ## 1 + n(1 + INSIDE + 1)
         if a % i == 0 and b % i == 0: ## 1 + 1 + 2 + 1 + 1 + 2
@@ -20,6 +22,29 @@ def mcd_euclides(a, b):
 ## 2 + 1 + n(1 + 1 + 1) + 1 = 4 + 3n
 ## Better Case
 ## 2 + 1 + 1 = 4
+
+
+def compare_euclides(a, b):
+    start = time.perf_counter()
+    mcd_euclides(a, b)
+    end = time.perf_counter()
+    return end - start
+
+def compare_fuerza_bruta(a, b):
+    start = time.perf_counter()
+    mcd_fuerza_bruta(a, b)
+    end = time.perf_counter()
+    return end - start
+
+def lower_time():
+    if(compare_fuerza_bruta(10,10) < compare_euclides(10,10)): 
+        return "Fuerza Bruta"
+    else:
+        return "Euclides"
+
+print("Euclides:", compare_euclides(10,10))
+print("Fuerza Bruta:", compare_fuerza_bruta(10,10))
+print("The lower alghoritm:", lower_time())
 
 
 ##Todo: Compare these algorithms with the time module
