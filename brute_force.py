4abd0bb63ab0d55af03d94bc3a4c13eb940e133f
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-08-16 20:00:50 -0500

    chore: initial config

---
+++ brute_force.py
@@ -0,0 +1,22 @@
+def mcd_fuerza_bruta(a, b):
+    menor = min(a, b) # 1 + 1
+    for i in range(menor, 0, -1): ## 1 + n(1 + INSIDE + 1)
+        if a % i == 0 and b % i == 0: ## 1 + 1 + 2 + 1 + 1 + 2
+            return i ## 1
+    return 1 ## 1
+
+## Worst and Average Case
+## 2 + 1 + n( 1 + 1 + 2 + 1 + 1 + 2) + 1 = 4 + 8n
+## Better Case
+## 1 + 1 + n(1 + 1 + 1) = 2 + 3n
+
+
+def mcd_euclides(a, b):
+        while b != 0:  ## 1 + 2 + n(INSIDE)
+            a, b = b, a % b ## 1 + 1
+        return a ## 1
+    
+## Worst and Average Case
+## 2 + 1 + n(1 + 1 + 1) + 1 = 4 + 3n
+## Better Case
+## 2 + 1 + 1 = 4
